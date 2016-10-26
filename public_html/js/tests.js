2fa6caa9c4a5dd866def44fcc9e1aa4c907d2650
mmtadmin <mmtadmin@pc97.mee.tcd.ie>
2016-10-26 17:07:29 +0100

    Started standalone test variables- actions required

---
+++ public_html/js/tests.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Level 1 test, with 2 images
+function createTest(){
+    type: "l1",
+    image1: "dummy.bmp", image2: "dummy.bmp",
+    sound: "dummy.wav",
+    answer: "-1"
+}
+//Level 2 test, with 3 images
+var test2 = {
+    type: "l2", 
+    image1: "dummy.bmp", image2: "dummy.bmp", 
+    image3: "dummy.bmp", 
+    sound: "dummy.wav", 
+    answer: "-1"
+}
+//Level 3 test, with 4 images
+var test3 = {
+    type: "l13", 
+    image1: "dummy.bmp", image2: "dummy.bmp", 
+    image3: "dummy.bmp", image4: "dummy.bmp", 
+    sound: "dummy.wav", 
+    answer: "-1"
+}
