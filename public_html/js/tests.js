0be8ed341a9858427fcb116e3b0babe7072910e8
mmtadmin <mmtadmin@pc97.mee.tcd.ie>
2016-10-27 11:13:57 +0100

    rename nb project

--- public_html/js/tests.js
+++ public_html/js/tests.js
@@ -5,25 +5,25 @@
  */
 
 //Level 1 test, with 2 images
-function createTest(){
-    type: "l1",
-    image1: "dummy.bmp", image2: "dummy.bmp",
-    sound: "dummy.wav",
-    answer: "-1"
-}
-//Level 2 test, with 3 images
-var test2 = {
-    type: "l2", 
-    image1: "dummy.bmp", image2: "dummy.bmp", 
-    image3: "dummy.bmp", 
-    sound: "dummy.wav", 
-    answer: "-1"
-}
-//Level 3 test, with 4 images
-var test3 = {
-    type: "l13", 
-    image1: "dummy.bmp", image2: "dummy.bmp", 
-    image3: "dummy.bmp", image4: "dummy.bmp", 
-    sound: "dummy.wav", 
-    answer: "-1"
-}
+//function createTest(){
+//    type: "l1",
+//    image1: "dummy.bmp", image2: "dummy.bmp",
+//    sound: "dummy.wav",
+//    answer: "-1"
+//}
+////Level 2 test, with 3 images
+//var test2 = {
+//    type: "l2", 
+//    image1: "dummy.bmp", image2: "dummy.bmp", 
+//    image3: "dummy.bmp", 
+//    sound: "dummy.wav", 
+//    answer: "-1"
+//}
+////Level 3 test, with 4 images
+//var test3 = {
+//    type: "l13", 
+//    image1: "dummy.bmp", image2: "dummy.bmp", 
+//    image3: "dummy.bmp", image4: "dummy.bmp", 
+//    sound: "dummy.wav", 
+//    answer: "-1"
+//}
